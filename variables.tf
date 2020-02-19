6c0c2d28b917c63ba0f486c6661e072c84a79eb3
Vincent RABAH <vincent.rabah@gmail.com>
2020-02-19 17:41:52 +0100

    update urls

--- variables.tf
+++ variables.tf
@@ -2,22 +2,17 @@
 variable os_name {
   type = map
   default  = {
-    debian = 0
-    ubuntu = 1
-    centos = 2
-    alpine = 3
-    openbsd= 4
+    debian      = 0
+    ubuntu      = 1
+    centos      = 2
   }
 }
 
 variable os_src {
   type = list
   default = [
-    {iso_src="https://"},
-    {iso_src="https://"},
-    {iso_src="https://"},
-    {iso_src="https://"},
-    {iso_src="https://"},
-    {iso_src="https://"}
+    {iso_src="https://cdimage.debian.org/cdimage/openstack/current-10/debian-10-openstack-amd64.qcow2"},
+    {iso_src="https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"},
+    {iso_src="https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"},
   ]
 }
